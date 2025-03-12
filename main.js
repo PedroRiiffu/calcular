57a60ac396d5907c4ad4fcb276993372d35a28fc
PedroRiiffu <pedrosantosbu@gmail.com>
2025-03-12 13:12:40 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const valorX = document.getElementById('numberX');
 const valorY = document.getElementById('numberY');
 const form = document.getElementById('form-calcular');
 
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+} )
+
 function compararNumero(){
     var inputNumberX = document.getElementById("numberX").value;
     var inputNumberY = document.getElementById("numberY").value;
@@ -14,14 +18,15 @@ function compararNumero(){
             alert("os numeros sao iguais.");
         } else if (numberX > numberY) {
             alert("o primeiro numero é maior");
-        } else {
+        } else (numberX < numberY) {
             alert ("o segundo numero é maior");
-        } 
-        else {
             alert("porfavor, digite numeros inteiros validos")
         }
         } 
+        } 
     }
+
+    console.log(form);
 
 
 
